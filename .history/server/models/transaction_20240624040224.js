f8393bf71d5b952aa201ce6a7bb637ac2edce9e9
JuTeLabs <julietech3@gmail.com>
2024-06-24 04:04:25 +0100

    made some changes in the models

---
+++ .history/server/models/transaction_20240624040224.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const TransactionSchema = new mongoose.Schema({
+    accountId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account', required: true },
+    type: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: Date, default: Date.now }
+},{time});
+const transactModel = mongoose.model('Transaction', TransactionSchema);
+module.exports = transactModel;
